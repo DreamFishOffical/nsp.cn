ee2509b37875822069cc35c41042981881b75e50
zhao114514 <157360032+zhao114514@users.noreply.github.com>
2024-01-23 18:48:38 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -4,5 +4,12 @@ int gamemode;
 int set;
 
 int main(){
-    
+    cout<<"欢迎来到查找数字若智小游戏！"<<endl;
+    cout<<"请按任意键继续..."<<endl;
+    char fuck=getch();
+    for(int i=1;i<=114514;++i){
+        printf("\n");
+    }
+    cout<<"请选择你的难度：1为简单(1-100)，2为中等(1-1000)，3为困难(1-10000)，4为大闲者(1-114514)"<<endl;
+    char 
 }
